0b1206d8f861c9feec9df65c0563966b2b287ace
tomszociepa <tom.ociepa@gmail.com>
2023-08-16 09:30:56 +0200

    init project

---
+++ src/main/java/com/tomasz/springsecuritypodstawa/SpringSecurityPodstawaApplication.java
@@ -0,0 +1,13 @@
+package com.tomasz.springsecuritypodstawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPodstawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPodstawaApplication.class, args);
+	}
+
+}
